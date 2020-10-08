4b72c219fb213e9f90de89acf4c14dd24b14f5b1
Arthur Sandro <arthurcs426@gmail.com>
2020-10-07 17:50:56 -0700

    just adding more react syntax

--- src/apexApp.js
+++ src/apexApp.js
@@ -2,10 +2,14 @@ import React, { Component } from 'react';
 import { Container, Col, Row, Jumbotron, Form, Button } from 'react-bootstrap';
 import './zenva-academy.png';
 
-class Dog extends Component {
+class ApexForm extends Component {
   render() {
     return (
-      
+      <div>
+        Hello from ApexForm!
+      </div>
     );
   }
 }
+
+export default ApexForm;
