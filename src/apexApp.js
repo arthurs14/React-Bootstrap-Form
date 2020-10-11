e568f2a994fabd65bf8902e22706e64d9670e10b
Arthur Sandro <arthurcs426@gmail.com>
2020-10-11 14:42:26 -0700

    fixed exports and working on form using react-bootstrap

--- src/apexApp.js
+++ src/apexApp.js
@@ -1,13 +1,46 @@
 import React, { Component } from 'react';
 import { Container, Col, Row, Jumbotron, Form, Button } from 'react-bootstrap';
-import './zenva-academy.png';
+import logo from './zenva-academy.png';
 
 class ApexForm extends Component {
+  state = {
+    username: '',
+  };
+
   render() {
+    const { username } = this.state;
+
     return (
-      <div>
-        Hello from ApexForm!
-      </div>
+      <Container>
+        <Row>
+          <Col med={{ span: 7, offset: 3 }}>
+            <Jumbotron>
+              <p>
+                Welcome to Apex Legends. Fill out some information about the normal mode and third person mode so that we can improve the game over time!
+              </p>
+
+              <Form onSubmit={this.handleSubmit}>
+                <Form.Group>
+                  <h2>User Information</h2>
+                  <Form.Label>Username</Form.Label>
+                  <Form.Control
+                    type="input"
+                    value={username} placeholder="Username"
+                    onChange={this.setUsername}
+                  />
+                </Form.Group>
+
+                <Form.Group controlId="formBirth">
+                  <Form.Label>Birthday</Form.Label>
+                  <Form.Control type="date" />
+                </Form.Group>
+
+
+              </Form>
+            </Jumbotron>
+          </Col>
+        </Row>
+      </Container>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import ApexApp from './apexApp';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ApexApp />
   </React.StrictMode>,
   document.getElementById('root')
 );
