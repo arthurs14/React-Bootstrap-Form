f5bc364192db82c691cbcad32bc6ecef53effbb6
Arthur Sandro <arthurcs426@gmail.com>
2020-10-11 17:10:57 -0700

    added battleroyaleform as a component to hold the form instead of having it all in one main class

---
+++ src/ApexForm.js
@@ -0,0 +1,72 @@
+import React, { Component } from 'react';
+import { Container, Col, Row, Jumbotron, Form, Button } from 'react-bootstrap';
+import BattleRoyaleForm from './BattleRoyaleForm';
+import logo from './zenva-academy.png';
+
+class ApexForm extends Component {
+  state = {
+    username: '',
+    date: '',
+    sex: '',
+    race: '',
+  };
+
+  setUsername = (ev) => {
+    this.setState({
+      username: ev.target.value,
+    });
+  }
+
+  setDate = (ev) => {
+    this.setState({
+      date: ev.target.value,
+    });
+  }
+
+  setSex = (ev) => {
+    this.setState({
+      sex: ev.target.value,
+    });
+  }
+
+  setRace = (ev) => {
+    this.setState({
+      race: ev.target.value,
+    });
+  }
+
+  handleSubmit = (ev) => {
+    ev.preventDefault();
+    const { username, date } = this.state;
+
+    alert(`Response Submitted for ${username} on ${date}`);
+  }
+
+  render() {
+    return (
+      <Container>
+        <Row>
+          <Col med={{ span: 7, offset: 3 }}>
+            <Jumbotron>
+              <img width={100} src={logo} alt="logo" />
+              <p>
+                Welcome to Apex Legends. Fill out some information about the battle royale mode and third person mode so that we can improve the game over time!
+              </p>
+
+              <BattleRoyaleForm
+                formData={this.state}
+                setUsername={this.setUsername}
+                setDate={this.setDate}
+                setSex={this.setSex}
+                setRace={this.setRace}
+                handleSubmit={this.handleSubmit}
+              />
+            </Jumbotron>
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
+}
+
+export default ApexForm;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import ApexApp from './apexApp';
+import ApexForm from './ApexForm';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <ApexApp />
+    <ApexForm />
   </React.StrictMode>,
   document.getElementById('root')
 );
